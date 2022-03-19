bee640fb5ed6cf91e498072f59622a620fe4b8b2
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2022-03-19 16:16:47 -0300

    add post user controller

---
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -0,0 +1,46 @@
+package com.ead.authuser.controllers;
+
+import com.ead.authuser.dtos.UserDto;
+import com.ead.authuser.enums.UserStatus;
+import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/api/v1/auth")
+public class AuthenticationController {
+
+    private final UserService userService;
+
+    public AuthenticationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Object> registerUser(@RequestBody UserDto request) {
+        if (userService.existsByUsername(request.getUsername())) {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body("Error: Username is already taken!");
+        }
+        if (userService.existsByEmail(request.getEmail())) {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body("Error: Email is already taken!");
+        }
+
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(request, userModel);
+        userModel.setUserStatus(UserStatus.ACTIVE);
+        userModel.setUserType(UserType.STUDENT);
+        userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+        userService.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
+    }
+}
--- src/main/java/com/ead/authuser/services/UserService.java
+++ src/main/java/com/ead/authuser/services/UserService.java
@@ -11,4 +11,7 @@ public interface UserService {
     List<UserModel> findAll();
     Optional<UserModel> findById(UUID id);
     void deleteUser(UserModel userModel);
+    void save(UserModel userModel);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
