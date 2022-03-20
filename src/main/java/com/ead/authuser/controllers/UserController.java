717eadf7a76108d7bc5f7d096b6765d6352b2810
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2022-03-20 17:08:26 -0300

    add hateoas for user model

--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -19,6 +19,9 @@ import java.time.ZoneId;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api/v1/users")
@@ -34,7 +37,15 @@ public class UserController {
     public ResponseEntity<Page<UserModel>> getAllUsers(
             SpecificationTemplate.UserSpec spec,
             @PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable) {
-        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll(spec, pageable));
+
+        Page<UserModel> userModelPage = userService.findAll(spec, pageable);
+        if (userModelPage.hasContent()) {
+            for (UserModel model : userModelPage.toList()) {
+                model.add(linkTo(methodOn(UserController.class).getUserById(model.getUserId())).withSelfRel());
+            }
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(userModelPage);
     }
 
     @GetMapping("/{id}")
