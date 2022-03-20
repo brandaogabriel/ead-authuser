2f120d79a58a5337ae52485b0939bcfcfecf6830
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2022-03-20 16:46:15 -0300

    add jpa specification

--- src/main/java/com/ead/authuser/services/UserService.java
+++ src/main/java/com/ead/authuser/services/UserService.java
@@ -3,17 +3,22 @@ package com.ead.authuser.services;
 import com.ead.authuser.models.UserModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserService {
 
-    Page<UserModel> findAll(Pageable pageable);
+    Page<UserModel> findAll(Specification<UserModel> spec, Pageable pageable);
+
     Optional<UserModel> findById(UUID id);
+
     void deleteUser(UserModel userModel);
+
     void save(UserModel userModel);
+
     boolean existsByUsername(String username);
+
     boolean existsByEmail(String email);
 }
--- src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.UserService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,8 +22,8 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public Page<UserModel> findAll(Pageable pageable) {
-        return userRepository.findAll(pageable);
+    public Page<UserModel> findAll(Specification<UserModel> spec, Pageable pageable) {
+        return userRepository.findAll(spec, pageable);
     }
 
     @Override
