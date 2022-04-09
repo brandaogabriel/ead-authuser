6614ae05a0aa686e194fc5331d85a5424e5d41e3
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2022-04-09 13:34:06 -0300

    create api call to course microservice

---
+++ src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -0,0 +1,50 @@
+package com.ead.authuser.clients;
+
+import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.ResponsePageDto;
+import com.ead.authuser.services.UtilsService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.UUID;
+
+@Log4j2
+@Component
+public class CourseClient {
+
+    private final RestTemplate restTemplate;
+    private final UtilsService utilsService;
+
+
+    public CourseClient(RestTemplate restTemplate, UtilsService utilsService) {
+        this.restTemplate = restTemplate;
+        this.utilsService = utilsService;
+    }
+
+    public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
+        List<CourseDto> fetchResult = null;
+        ResponseEntity<ResponsePageDto<CourseDto>> result = null;
+        String url = utilsService.createUrl(userId, pageable);
+
+        log.debug("Request URL: {}", url);
+        log.info("Request URL: {}", url);
+        try {
+            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<>(){};
+            result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            fetchResult = result.getBody().getContent();
+            log.debug("Response Number of elements: {}", fetchResult.size());
+        } catch (HttpStatusCodeException e) {
+            log.error("Error request /api/v1/courses {}", e);
+        }
+        log.info("Ending request /api/v1/courses userId {}", userId);
+        return result.getBody();
+    }
+}
