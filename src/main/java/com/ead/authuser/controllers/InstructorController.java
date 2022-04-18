d7ea4a845af41440bca09c4e441860ff243a5731
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2022-04-18 15:22:21 -0300

    create endpoint to change user from STUDENT to INSTRUCTOR

---
+++ src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -0,0 +1,42 @@
+package com.ead.authuser.controllers;
+
+import com.ead.authuser.dtos.InstructorDto;
+import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.UserService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
+
+@Log4j2
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/api/v1/instructors")
+public class InstructorController {
+
+    private final UserService userService;
+
+    public InstructorController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/subscription")
+    public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDto request) {
+        Optional<UserModel> possibleUserModel = userService.findById(request.getUserId());
+        if (possibleUserModel.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        } else {
+            var userModel = possibleUserModel.get();
+            userModel.setUserType(UserType.INSTRUCTOR);
+            userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+            userService.save(userModel);
+            return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
+        }
+    }
+}
